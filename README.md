5eee395b80b9da83e4f81ebecea094fda5b7878b
Kingsword <kingsword09@gmail.com>
2024-11-01 15:08:33 +0800

    :bug: fix android error

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ### Project created using [KotlinDslExample](https://github.com/kingsword09/KotlinDslExample) template.
 
-- react-native 0.76.1
+- react-native ^0.76.1
 #### package manager
-- bun 1.1.33
+- bun ^1.1.33
 
 #### state manager
-- zustand 5.0.1
+- zustand ^5.0.1
 
 #### key/value storage
-- react-native-mmkv 3.1.0
+- react-native-mmkv ^3.1.0
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("com.facebook.react.rootproject")
 }
 
+extra["buildToolsVersion"] = libs.versions.buildToolsVersion.get()
+extra["minSdkVersion"] = libs.versions.minSdkVersion.get().toInt()
+extra["compileSdkVersion"] = libs.versions.compileSdkVersion.get().toInt()
+extra["targetSdkVersion"] = libs.versions.targetSdkVersion.get().toInt()
+extra["ndkVersion"] = libs.versions.ndkVersion.get()
+
 buildscript {
     repositories {
         google()
