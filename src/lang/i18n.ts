a25b881b8af67d12277ce76ca6a7c511a933f187
Kingsword <kingsword09@gmail.com>
2024-11-04 17:23:32 +0800

    :ambulance:

--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -1,20 +1,32 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import {getLocales} from 'react-native-localize';
+import {findBestLanguageTag} from 'react-native-localize';
 
 import en from './en/en.json';
 import zh from './zh/zh.json';
+
+const translationGetters = {
+  en: () => import('~/lang/en/en.json'),
+  zh: () => import('~/lang/zh/zh.json'),
+};
+
+const getCurrentLanguage = () => {
+  const fallback = {languageTag: 'en', isRTL: false};
+  const languageConfig = findBestLanguageTag(Object.keys(translationGetters));
+  return languageConfig ?? fallback;
+};
 
 const resources = {
   en: en,
   zh: zh,
 };
-const lng = getLocales()?.[0]?.languageCode ?? 'en';
+const {languageTag} = getCurrentLanguage();
 
 i18n.use(initReactI18next).init({
   //
+  compatibilityJSON: 'v3',
   resources,
-  lng,
+  lng: languageTag,
   fallbackLng: 'en',
 });
 
